f263d6c15c8268249559537e12c09e9efadddb43
jvdalaglio <jvdalaglio@gmail.com>
2022-11-04 10:55:36 -0300

    Criando arquivo css

--- Pokedex/src/main.tsx
+++ Pokedex/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css"
 import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
