69bb808841dd7543e745aed3f3ce7c4599f2988f
jvdalaglio <jvdalaglio@gmail.com>
2022-11-07 08:50:48 -0300

    definindo estilos da navbar

---
+++ Pokedex/src/components/Nav/Nav.tsx
@@ -0,0 +1,10 @@
+import "./Nav.css"
+
+export function Nav() {
+  return (
+    <nav className="nav">
+      <a href="#" className="brand">Pokédex</a>
+      <a href="#" className="btn-goBack">voltar</a>
+    </nav>
+  )
+}
--- Pokedex/src/pages/Home.tsx
+++ Pokedex/src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { Nav } from "../components/Nav/Nav";
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <Nav />
+  );
 }
