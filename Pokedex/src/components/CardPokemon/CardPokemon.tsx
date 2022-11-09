0b8d2d51dacce56678d5e6a8f77f322fd9eb9913
jvdalaglio <jvdalaglio@gmail.com>
2022-11-09 10:40:30 -0300

    passando os tipos dos pokemons para o card

--- Pokedex/src/components/CardPokemon/CardPokemon.tsx
+++ Pokedex/src/components/CardPokemon/CardPokemon.tsx
@@ -16,12 +16,13 @@ type CardPokemonProps = {
 export function CardPokemon(props: CardPokemonProps) {
   return (
     <Link to={`/details/${props.id}`}>
-      <div className={`card type--${props.types[0].type.name.toLowerCase()}`}>
+      <div className={`card type--${props.types[0].type.name.toLowerCase}`}>
         <div className="info">
             <span className="info__number">#{props.id.toString().padStart(3,"0")}</span>
             <p className="info__name">{props.name}</p>
-
-            <p>{}</p>
+            {props.types.map((item) => {
+              return <p>{item.type.name}</p>
+            })}
         </div>
         <img className="card__img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`} alt={props.name} />
       </div>
