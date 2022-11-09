919ac97e72581431a533d7a0ff716fe2c27c836e
jvdalaglio <jvdalaglio@gmail.com>
2022-11-09 08:33:37 -0300

    definindo link para os cards

--- Pokedex/src/components/CardPokemon/CardPokemon.tsx
+++ Pokedex/src/components/CardPokemon/CardPokemon.tsx
@@ -7,12 +7,14 @@ type CardPokemonProps = {
 
 export function CardPokemon(props: CardPokemonProps) {
   return (
-    <div className="card">
-          <div className="info">
+    <a href={`/details/${props.id}`}>
+      <div className="card">
+        <div className="info">
             <span className="info__number">#{props.id.toString().padStart(3,"0")}</span>
             <p className="info__name">{props.name}</p>
-          </div>
-          <img className="card__img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`} alt={props.name} />
         </div>
+        <img className="card__img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`} alt={props.name} />
+      </div>
+    </a>
   );
 }
--- Pokedex/src/router/router.tsx
+++ Pokedex/src/router/router.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <Home /> ,
   },
   {
-    path: "details",
+    path: `details/:id`,
     element: <Details /> ,
   },
 ]);
