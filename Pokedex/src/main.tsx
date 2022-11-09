850e7955669f89df9e4fd58336d4e19ec2fa939d
jvdalaglio <jvdalaglio@gmail.com>
2022-11-09 08:27:33 -0300

    configurando rotas

--- Pokedex/src/main.tsx
+++ Pokedex/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import "./index.css"
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
+import {RouterProvider} from "react-router-dom"
+import {router} from "./router/router"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <section className='container'>
-    <Home />
+    <RouterProvider router={router}/>
     </section>
   </React.StrictMode>
 );
---
+++ Pokedex/src/router/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom"
+import { Home } from "../pages/Home";
+import { Details } from "../pages/Details";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home /> ,
+  },
+  {
+    path: "details",
+    element: <Details /> ,
+  },
+]);
