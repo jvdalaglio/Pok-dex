c3f6e2bb2ee1b01753878c40ce96870bee112eaa
jvdalaglio <jvdalaglio@gmail.com>
2022-11-09 10:00:03 -0300

    instalando @types/react-router-dom

--- Pokedex/src/pages/Details.tsx
+++ Pokedex/src/pages/Details.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom"
 import { Nav } from "../components/Nav/Nav";
+import PokemonList from "../assets/pokemon.json"
 
 export function Details() {
+  const {id} = useParams();
+  const {pokemons} = PokemonList;
+ 
   return (
+    <>
     <Nav hasGoBack={true} />
+
+    <h1>{pokemons.find((pokemon) =>  String(pokemon.id) === id)?.name}</h1>
+    </>
   );
 }
