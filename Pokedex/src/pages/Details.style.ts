cc9bc01f76beaa3eaca65b788e6470270d6c0f19
jvdalaglio <jvdalaglio@gmail.com>
2022-11-21 10:34:44 -0300

    Criando página details

--- Pokedex/src/pages/Details.style.ts
+++ Pokedex/src/pages/Details.style.ts
@@ -1,89 +1,106 @@
 import styled from "styled-components";
 
+export const Container = styled.div` 
+  height: calc(100vh - 4rem - 40px);
+  display: flex;
+
+`
+
+export const Image = styled.img`
+
+`
+
 export const Card = styled.div`
-  width: 100%;
-  width: 330px;
-  min-height: 115px;
-  background-color: #ddd;
-  border-radius: 10px;
-  padding: 20px;
+border: 4px solid #62b957;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 20px;
+padding: 50px;
 
-  position: relative;
-
-.info__number{
+.info__name {
   font-weight: 700;
-  font-size: 12px;
-  line-height: 16px;
-  color: rgba(23, 27, 24, 0.6);
-}
-.info__name{
-  font-weight: 700;
-  font-size: 26px;
-  line-height: 34px;
-  color: #ffffff;
-  margin-bottom: 8px;
-}
-.card__img{
-  max-width: 100%;
-  max-height: 140px;
-  position: absolute;
-  top: -35px;
-  right: 10px;
+  font-size: 64px;
+  line-height: 83px;
+  color: #FFFFFF;
 }
 
+.info__number {
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 31px;
+  color: rgba(23, 23, 27, 0.6);
+}
 &.type--bug {
   background-color: #8bd674;
+  border-color: #8cb230;
 }
 &.type--dark {
   background-color: #6f6e78;
+  border-color: #58575f;
 }
 &.type--dragon {
   background-color: #7383b9;
+  border-color: #0f6ac0;
 }
 &.type--electric {
   background-color: #f2cb55;
+  border-color: #eed535;
 }
 &.type--fairy {
   background-color: #eba8c3;
+  border-color: #ed6ec7;
 }
 &.type--fighting {
   background-color: #eb4971;
+  border-color: #d04164;
 }
 &.type--fire {
   background-color: #ffa756;
+  border-color: #fd7d24;
 }
 &.type--flying {
   background-color: #83a2e3;
+  border-color: #748fc9;
 }
 &.type--ghost {
   background-color: #8571be;
+  border-color: #556aae;
 }
 &.type--grass {
   background-color: #8bbe8a;
+  border-color: #62b957;
 }
 &.type--ground {
   background-color: #f78551;
+  border-color: #dd7748;
 }
 &.type--ice {
   background-color: #91d8df;
+  border-color: #61cec0;
 }
-&.type--normal {
+&.type--noemal {
   background-color: #b5b9c4;
+  border-color: #9da0aa;
 }
 &.type--poison {
   background-color: #9f6e97;
+  border-color: #a552cc;
 }
 &.type--psychic {
   background-color: #ff6568;
+  border-color: #a552cc;
 }
 &.type--rock {
   background-color: #d4c294;
+  border-color: #baab82;
 }
 &.type--steel {
   background-color: #4c91b2;
+  border-color: #4a90da;
 }
 &.type--water {
   background-color: #58abf6;
+  border-color: #4a90da;
 }
-`
 
+
+`
--- Pokedex/src/pages/Details.tsx
+++ Pokedex/src/pages/Details.tsx
@@ -1,17 +1,59 @@
 import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom"
 import { Nav } from "../components/Nav/Nav";
-import PokemonList from "../assets/pokemon.json"
+import { Container, Image, Card } from "./Details.style"
+import { Badge } from "../components/Badge/Badge";
+import { CardPokemonProps } from "../components/CardPokemon/CardPokemon";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading/Loading";
 
 export function Details() {
   const {id} = useParams();
-  const {pokemons} = PokemonList;
- 
+  const[isLoading, setIsLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState<CardPokemonProps>({} as CardPokemonProps);
+
+  async function getPokemonData() {
+    const {data} = await api.get(`/pokemon/${id}`);
+    setPokemonData({
+      id: data.id,
+      name: data.name,
+      types: data.types,
+    });
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <>
     <Nav hasGoBack={true} />
+    <Container>
 
-    <h1>{pokemons.find((pokemon) =>  String(pokemon.id) === id)?.name}</h1>
+    <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+     alt={pokemonData.name}
+      />
+
+    <Card className={`type--${pokemonData.types[0].type.name.toLowerCase()}`}>
+      <>
+          <span className="info__number">#{String(id).padStart(3,"0")}</span>
+          <p className="info__name">{pokemonData.name}</p>
+          {pokemonData.types.map((item, index) => {
+            return <Badge key={index} name={item.type.name} />
+          })}
+      </>
+    </Card>
+
+
+    </Container>
+
     </>
   );
 }
