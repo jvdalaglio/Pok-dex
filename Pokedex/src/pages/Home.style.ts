13f800b8dfab4ed42f55fbf54a98f657bbda8fc5
jvdalaglio <jvdalaglio@gmail.com>
2022-11-21 09:29:37 -0300

    Ajustando responsividade

--- Pokedex/src/pages/Home.style.ts
+++ Pokedex/src/pages/Home.style.ts
@@ -11,10 +11,18 @@ export const Title = styled.h1 `
 `
 
 export const List = styled.div `
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  row-gap: 50px;
-  column-gap: 30px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 30px;
+    column-gap: 30px;
+
+  @media only screen and (max-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media only screen and (max-width: 700px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 export const Input = styled.input`
