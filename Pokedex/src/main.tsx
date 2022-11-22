648b413c53c00c3fa3f2ce524e73cd4ab74e9884
jvdalaglio <jvdalaglio@gmail.com>
2022-11-22 07:38:43 -0300

    Instalando redux toolkit

--- Pokedex/src/main.tsx
+++ Pokedex/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./index.css"
-import { Details } from './pages/Details'
-import { Home } from './pages/Home'
 import {RouterProvider} from "react-router-dom"
 import {router} from "./router/router"
+import {Provider} from "react-redux"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
